ecea6d4277de10a6600e96078d06a58175aae31e
shidehrzf <shideh.rezaeifar@unige.ch>
2019-08-28 14:12:46 +0200

    bug fixed

---
+++ hw1/PixelCNN_model.py
@@ -0,0 +1,96 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+class MaskedConv2d(nn.Conv2d):
+    def __init__(self, mask_type, *args, **kwargs):
+        super(MaskedConv2d, self).__init__(*args, **kwargs)
+        self.mask_type = mask_type
+        self.register_buffer('mask', self.weight.data.clone())
+        _, _, kernel_height, kernel_width = self.weight.size()
+        self.mask.fill_(0)
+        half_h, half_w = kernel_height // 2, kernel_width // 2
+
+        self.mask[:, :, :half_h, :] = 1.0
+        self.mask[:, :, half_h, :half_w] = 1.0
+        if self.mask_type == 'A':
+            self.mask[:, :, half_h, half_w] = 0.0
+        else:
+            self.mask[:, :, half_h, half_w] = 1.0
+
+    def forward(self, x):
+        self.weight.data *= self.mask
+        return super(MaskedConv2d, self).forward(x)
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, h):
+        super(ResidualBlock, self).__init__()
+        self.h = h
+        # todo: set padding to same
+        self.network = []
+        self.network.extend([
+            nn.Conv2d(self.h, self.h // 2, (1, 1)),
+            nn.BatchNorm2d(self.h // 2),
+            nn.ReLU()
+        ])
+        self.network.extend([
+            MaskedConv2d('B', self.h // 2, self.h // 2, (3, 3), padding=1),
+            nn.BatchNorm2d(self.h // 2),
+            nn.ReLU()
+        ])
+
+        self.network.extend([
+            nn.Conv2d(self.h // 2, self.h, (1, 1)),
+            nn.BatchNorm2d(self.h),
+            nn.ReLU()
+        ])
+
+        self.network = nn.Sequential(*self.network)
+
+    def forward(self, x):
+        skip = x
+        x = self.network(x)
+        return F.relu(x + skip)
+
+
+class PixelCNN(nn.Module):
+    def __init__(self, in_channels):
+        super(PixelCNN, self).__init__()
+
+        self.in_channels = in_channels
+
+        self.network = [nn.BatchNorm2d(3)]
+
+        # 7x7 Conv input, type A
+        self.network.extend([
+            MaskedConv2d('A', 3, self.in_channels, (7, 7), padding=3),
+            nn.BatchNorm2d(self.in_channels),
+            nn.ReLU()
+        ])
+
+        self.network.extend(
+            [ResidualBlock(self.in_channels) for _ in range(12)]
+        )
+
+        #         # 3x3 Conv input, type B
+        #         self.network.extend([
+        #             MaskedConv2d('B', self.in_channels, self.in_channels, (3, 3), padding=1),
+        #             nn.BatchNorm2d(self.in_channels),
+        #             nn.ReLU(),
+        #         ])
+
+        #         # 1x1 Conv input
+        self.network.extend([
+            nn.Conv2d(self.in_channels, self.in_channels, (1, 1)),
+            nn.BatchNorm2d(self.in_channels),
+            nn.ReLU()])
+        self.network.extend([nn.Conv2d(self.in_channels, 12, (1, 1))])
+
+        self.network = nn.Sequential(*self.network)
+
+
+    def forward(self, x):
+        x = self.network(x)
+        x = x.reshape(x.size(0) , 4, 3 , 28, 28)
+        return x
---
+++ hw1/PixelCNN_train.py
@@ -0,0 +1,109 @@
+import numpy as np
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+import utils
+from PixelCNN_model import PixelCNN
+
+import torch
+import torch.nn as nn
+
+from torch.utils.data import DataLoader
+
+
+import torchvision.transforms as T
+eps = np.finfo(float).eps
+
+import tqdm
+from torchsummary import summary
+import os
+
+os.environ["CUDA_VISIBLE_DEVICES"] = str(3)
+use_cuda = True
+use_cuda = use_cuda and torch.cuda.is_available()
+print(use_cuda)
+
+
+
+batch_size = 64
+train_valid, x_test = utils.load_data('mnist-hw1.pkl')
+x_train = train_valid[:int(len(train_valid)*0.8)]
+x_valid = train_valid[int(len(train_valid)*0.8):]
+
+transform = T.Compose([T.ToTensor()])
+
+dataloader_train = DataLoader(utils.CMNIST(x_train, transform=transform), batch_size=batch_size,
+                              shuffle=True, num_workers=4, pin_memory=True)
+
+dataloader_val = DataLoader(utils.CMNIST(x_valid, transform=transform), batch_size=batch_size,
+                              shuffle=True, num_workers=4, pin_memory=True)
+
+dataloader_test = DataLoader(utils.CMNIST(x_test, transform=transform), batch_size=batch_size,
+                              shuffle=True, num_workers=4, pin_memory=True)
+
+
+def train_validate(model, dataloader, optim, loss_fn, train):
+    model.train() if train else model.eval()
+    total_loss = 0
+    for batch_idx, x in enumerate(dataloader):
+
+
+        x = x.float().cuda() if use_cuda else x.float()
+
+        x_hat = model(x)
+        loss = loss_fn(x_hat, x.long())
+        if batch_idx % 50 == 0:
+            print('\n batch:{} ---- loss:{}'.format(batch_idx, loss.item()))
+
+        if train:
+            optim.zero_grad()
+            loss.backward()
+            optim.step()
+
+        total_loss += loss.item()
+    return total_loss / len(dataloader.dataset)
+
+model = PixelCNN(128)
+model = model.cuda() if use_cuda else model
+summary(model, input_size=(3,28,28))
+model.apply(utils.init_weights)
+
+optim = torch.optim.Adam(model.parameters(), 1e-3, weight_decay=1e-4)
+scheduler = torch.optim.lr_scheduler.StepLR(optim, step_size=45, gamma=0.1)
+
+# loss_fn = F.cross_entropy
+loss_fn = nn.CrossEntropyLoss(reduction='mean')
+n_epochs = 30
+
+train_loss = []
+val_loss = []
+best_loss = np.inf
+
+for epoch in tqdm.tqdm_notebook(range(0, n_epochs)):
+    scheduler.step(epoch)
+    t_loss = train_validate(model, dataloader_train, optim, loss_fn, train=True)
+    print('\n epoch:{} ---- training loss:{}'.format(epoch, t_loss))
+    train_loss.append(t_loss)
+
+    if epoch % 5 == 0:
+        v_loss = train_validate(model, dataloader_val, optim, loss_fn, train=False)
+        print('\n epoch:{} ---- validation loss:{}'.format(epoch, v_loss))
+        val_loss.append(v_loss)
+    if v_loss < best_loss:
+        best_loss = v_loss
+        print('Writing model checkpoint')
+        torch.save(model.state_dict(), 'models/pixelcnn_'+str(n_epochs)+ 'epochs_batch_'+str(batch_size)+'.pt')
+
+
+sns.set()
+plt.rcParams['figure.figsize'] = 5, 5
+plt.plot(np.arange(len(train_loss)), train_loss, label='train')
+plt.plot(np.arange(0, len(val_loss) * 5, 5), val_loss, label='val')
+plt.title('Training')
+plt.xlabel('Step')
+plt.ylabel('BCE')
+plt.legend()
+plt.grid(True)
+plt.savefig('Figs/training_'+str(n_epochs)+ 'epochs.png')
+
+
